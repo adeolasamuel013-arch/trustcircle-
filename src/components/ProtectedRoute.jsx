92d29200ca61be3be5f9295775c5e7b001627a70
adeolasamuel013-arch <adeolasamuel013@gmail.com>
2026-03-19 10:56:04 +0100

    Delete src directory

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
-
-export default function ProtectedRoute({ children }) {
-  const { user, loading } = useAuth()
-  if (loading) return <div className="page-loader"><div className="spinner" style={{ width: 32, height: 32 }}></div></div>
-  if (!user) return <Navigate to="/login" replace />
-  return children
-}
